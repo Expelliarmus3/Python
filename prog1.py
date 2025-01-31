9384a79b083e754c70fa0da6f98a6896f470e343
Ahana Sarkar <ahanasarkar66@gmail.com>
2025-01-31 23:56:05 +0530

    New commit

---
+++ prog1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 23:46:41 2025
+
+@author: Ahana
+"""
+
+n= int(input('Enter a number'))
+result=0
+for i in range(1, n):
+     result+=i**2   
+print('The result is ',result)
