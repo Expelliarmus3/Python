418b94ac062373a4189fc1ea65c67770216dae80
Ahana Sarkar <ahanasarkar66@gmail.com>
2025-02-01 10:43:20 +0530

    Revising

---
+++ Discount.py
@@ -0,0 +1,3 @@
+n= float(input('Enter your amount: \n'))
+n= n-(10/100)*n
+print("The net amount to be paid",n)
---
+++ If-Else.py
@@ -0,0 +1,9 @@
+n=input('Enter a number:')
+if(n==1):
+    print("Have a good day!")
+else:
+    print("What a joyous day!")
+
+
+# else:
+#     print('Bye')
