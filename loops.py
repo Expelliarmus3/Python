6905a155813887dbfa371e581fd74a4960f56fcf
Ahana Sarkar <ahanasarkar66@gmail.com>
2025-02-01 13:15:08 +0530

    new commit

--- loops.py
+++ loops.py
@@ -23,11 +23,27 @@
 #         print('*',end=" ")
 #     print()
 
-def mystery(container):
-    result=[]
-    for i in range(len(container)):
-        if i%2 != 0:
-            result.append(container[i]*4)
-        else:
-            result.append(container[i]*(-1))
-    return result
+# def mystery(container):
+#     result=[]
+#     for i in range(len(container)):
+#         if i%2 != 0:
+#             result.append(container[i]*4)
+#         else:
+#             result.append(container[i]*(-1))
+#     return result
+
+n=22
+k=0
+a=[]
+b=0
+while(n!=0):
+    k=k+(n%10)
+    a.append(n%10)
+    n=n//10
+a.sort()
+for i in a:
+    b=b+i
+if(b!=k):
+    print("PYTHON IS FUN")
+else:
+    print("PYTHON IS BORING")
