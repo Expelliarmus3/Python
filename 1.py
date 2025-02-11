5f0f52f6298f81b3e1e909d2372ac79ce06a7286
Ahana Sarkar <ahanasarkar66@gmail.com>
2025-02-11 21:57:40 +0530

    New

---
+++ 1.py
@@ -0,0 +1,43 @@
+#Prog 1
+# message= input("Enter your msg:")
+# print(message)
+
+#Prog 2
+# n1=int(input("Enter 1:"))
+# n2=int(input("Enter 2:"))
+# n3=int(input("Enter 3:"))
+# sum= n1+n2+n3
+# print(sum)
+
+#Prog 3
+# length=int(input("Length:"))
+# Width=int(input("Width:"))
+# area=length*Width
+# print(area)
+
+#Prog 4
+# n1=int(input("1:"))
+# n2=int(input("2:"))
+# print(n1,n2)
+# n1=n1^n2
+# n2=n1^n2
+# n1=n1^n2
+# print(n1,n2)
+
+#Prog 5
+# n1= int(input("Enter"))
+# if n1%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#Prog 6
+n1=int(input())
+n2=int(input())
+n3=int(input())
+max=n1
+if n2>max:
+    max=n2
+if n3>max:
+    max=n3
+print(max)
