959458d2e8a20752796ba9c55063c08bacd19017
Ahana Sarkar <ahanasarkar66@gmail.com>
2025-02-22 09:57:34 +0530

    Nptel week 3

---
+++ Nptel1.py
@@ -0,0 +1,23 @@
+#LISTS
+# shopping=["Bread","Coffee","Sugar"]
+# print(shopping)
+# for i in shopping:
+#     print(i)
+# print()
+# for r in range(0,len(shopping)):
+#     print(shopping[r])
+# print()
+# shopping.append("Curd")
+# shopping.insert(0,"Egg")
+# for i in shopping:
+#     print(i)
+
+L = [1,22,35,0,-5,15,69,88,25,99,4,-988,67,58,25,41,59,102,15]
+print(L.count(15))
+L.sort()
+print(L)
+L.reverse()
+print(L)
+
+#SLICING
+print(L[1:20:2])
